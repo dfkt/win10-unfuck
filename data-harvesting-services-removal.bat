54f5f2f3824e05b7087b0d085bd0ec255fc7864b
Velocet <velocet@users.noreply.github.com>
2015-08-31 22:20:09 +0200

    More domains, M$ DNS whitelisting, Links, etc.
    Added some info about blocking with the HOSTS file and DNS whitelisting
    from Microsoft.
    Changed a paths to use variables for better support on none standard
    installations.
    Better PowerShell script for removal of build in AppxPackages.
    Corrected the path of Flash Player.

--- data-harvesting-services-removal.bat
+++ data-harvesting-services-removal.bat
@@ -6,6 +6,7 @@ REM For Windows 10
 REM Save this script as "DelSvcs.cmd"
 REM Run it as an administrator from an elevated command prompt.
 REM It deletes services that Microsoft uses to harvest some of your data.
+REM Always use variables...
 
 net stop DiagTrack
 net stop dmwappushservice
@@ -13,7 +14,7 @@ net stop Wecsvc
 sc delete dmwappushservice
 sc delete diagtrack
 sc delete Wecsvc
-cd c:\ProgramData\Microsoft\Diagnosis\ETLLogs\Autologger
+cd %ProgramData%\Microsoft\Diagnosis\ETLLogs\Autologger
 cacls Autologger-Diagtrack-Listener.etl /d SYSTEM
 
 pause
