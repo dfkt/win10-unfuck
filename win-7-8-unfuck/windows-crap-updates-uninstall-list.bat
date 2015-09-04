b6694f3c0d0c4b3b82b2c5f6216d0b5048b10242
Vocatus Gate <vocatus.gate@gmail.com>
2015-09-04 16:52:26 -0700

    Add /norestart and /quiet flags

--- win-7-8-unfuck/windows-crap-updates-uninstall-list.bat
+++ win-7-8-unfuck/windows-crap-updates-uninstall-list.bat
@@ -7,33 +7,33 @@
 :: TO GET RID OF THEM FOR GOOD, DISABLE/HIDE THEM IN WINDOWS UPDATE AFTER UNINSTALL.
 
 :: Compatibility update for upgrading Windows 7 
-wusa /uninstall /kb:2952664
+wusa /uninstall /kb:2952664 /norestart /quiet
 
 :: Compatibility update for Windows 8.1 and Windows 8
-wusa /uninstall /kb:2976978
+wusa /uninstall /kb:2976978 /norestart /quiet
 
 :: Update that enables you to upgrade from Windows 7 to a later version of Windows 
-wusa /uninstall /kb:2990214
+wusa /uninstall /kb:2990214 /norestart /quiet
 
 :: Update to Windows 7 SP1 for performance improvements
-wusa /uninstall /kb:3021917
+wusa /uninstall /kb:3021917 /norestart /quiet
 
 :: Update for customer experience and diagnostic telemetry
-wusa /uninstall /kb:3022345
+wusa /uninstall /kb:3022345 /norestart /quiet
 
 :: Update installs Get Windows 10 app in Windows 8.1 and Windows 7 SP1 
-wusa /uninstall /kb:3035583
+wusa /uninstall /kb:3035583 /norestart /quiet
 
 :: Update that enables you to upgrade from Windows 8.1 to Windows 10
-wusa /uninstall /kb:3044374
+wusa /uninstall /kb:3044374 /norestart /quiet
 
 :: Update for customer experience and diagnostic telemetry
-wusa /uninstall /kb:3068708
+wusa /uninstall /kb:3068708 /norestart /quiet
 
 :: Update that adds telemetry points to consent.exe in Windows 8.1 and Windows 7
-wusa /uninstall /kb:3075249
+wusa /uninstall /kb:3075249 /norestart /quiet
 
 :: Update for customer experience and diagnostic telemetry
-wusa /uninstall /kb:3080149
+wusa /uninstall /kb:3080149 /norestart /quiet
 
 exit
